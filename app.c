c465d7129f9ef6bb7ee004ad61f0e9e5dac9e895
Andreas Knudsen Alstrup <andreas090994@gmail.com>
2020-12-03 20:40:48 +0100

    Added weight to edges

--- app.c
+++ app.c
@@ -5,8 +5,9 @@
 
 /* Holder et hjørne */
 typedef struct node {
-    int vertexNum;                              // Hjørne nummer
-    struct node *next;                          // Holder adressem til næste node (vertex)
+    int vertexNum;                              // Knudepunkt nummer
+    struct node *next;                          // Holder adressen til næste node (vertex)
+    int capacity;                               // Kapaciteten fra knudepunktet
 } node;
 
 /* Holder adressen til den første node i listen */
@@ -20,7 +21,7 @@ typedef struct list {
 list *adjlist[MAX_NODE] = {0};
 
 
-void add_node(int s, int d);
+void add_node(int s, int d, int c);
 void print_list(void);
 
 int main(void) {
@@ -31,44 +32,46 @@ int main(void) {
         adjlist[i]->head = NULL;
     }
 
-    add_node(0,1);
-    add_node(0,2);
-    add_node(0,3);
+    add_node(0, 1, 5);
+    add_node(0, 2, 10);
+    add_node(0, 3, 15);
 
-    add_node(1,0);
-    add_node(1,2);
+    add_node(1, 0, 5);
+    add_node(1, 2, 15);
 
-    add_node(2,0);
-    add_node(2,1);
-    add_node(2,3);
+    add_node(2, 0, 5);
+    add_node(2, 1, 10);
+    add_node(2, 3, 15);
 
-    add_node(3,0);
-    add_node(3,2);
+    add_node(3, 0, 50);
+    add_node(3, 2, 500);
     
     print_list();
     return 0;
 }
 
 /* Tilføj node til graf, via naboliste */
-void add_node(int s, int d) {
+void add_node(int s, int d, int c) {
     node *dest, *tmp, *src;
 
     if (adjlist[s]->head == NULL) {         
         src = (node *)malloc(sizeof(node));     // laver node, source variabel opbevare adressen 
         src->vertexNum = s;                     // gir vertexNum værdien s i source node
         src->next = NULL;                       // gir next værdien NULL i source node
+        src->capacity = 0;                      // Sætter src node til 0 (skal måske havde en maksværdi)
         adjlist[s]->head = src;                 // gemmer indholdet i source, i adjlist index s 
     }
 
     dest = (node *)malloc(sizeof(node));        // laver ny node, destinations variabel opbebare adressen
     dest->vertexNum = d;                        // gir vertexNum værdien d i dest node
     dest->next = NULL;                          // gir next værdien NULL i dest node
+    dest->capacity = c;                         // Sætter kapaciteten mellem src og dest node
     tmp = adjlist[s]->head;                     // tmp variabel assignes værdien i head i adjlist[s]
-
+    
     while (tmp->next != NULL) {
         tmp = tmp->next;                        // hvis næste node ikke er NULL assignes tmp værdien til næste nodes værdi
     }
-    tmp->next = dest;                           // gemmer indholdet i destinations variabelen, i næste node
+    tmp->next = dest;                           // gemmer indholdet i destinations variablen, i næste node
 }
 
 /* Print visuel repræsentation af hvordan nodes er forbundet i nabolisten */
@@ -78,7 +81,7 @@ void print_list(void) {
         printf("\n Vetex %d\n: ", i);
 
         while (p) {
-            printf("%d -> ", p->vertexNum);
+            printf("[%d,%dw] -> ", p->vertexNum, p->capacity);
             p = p->next;
         }
     printf("\n");
