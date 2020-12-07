f874b6ca20384f3b23c2f83b37c354277c38183d
Archdragon <plaursen7.pl@gmail.com>
2020-12-07 12:42:00 +0100

    Update c-project1.c

--- c-project1.c
+++ c-project1.c
@@ -15,9 +15,9 @@ struct Edge {
 };
 
 void Edge(int from, int to, long capacity) {
-    this.from = from;
-    this.to = to;
-    this.capacity = capacity;
+    from = from;
+    to = to;
+    capacity = capacity;
 }
 
 long remainingCapacity(long capacity, long flow) {
